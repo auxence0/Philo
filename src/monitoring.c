be9f7f0cb016f6a592d01abb742ec96422867764
Auxence Sauvaget <asauvage@f6r11s15.clusters.42paris.fr>
2026-03-16 17:21:52 +0100

    le bitume a seche et la pluie reprend c'est ok tkt norme good

--- src/monitoring.c
+++ src/monitoring.c
@@ -6,41 +6,66 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 16:11:22 by asauvage          #+#    #+#             */
-/*   Updated: 2026/03/16 11:58:48 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/03/16 17:21:05 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	check_finish_eat(t_philo *philo)
+{
+	int	status;
+
+	status = 1;
+	pthread_mutex_lock(&philo->data->lock_eat);
+	if (philo->data->finish_eat == philo->data->nb_philo)
+		status = 0;
+	pthread_mutex_unlock(&philo->data->lock_eat);
+	return (status);
+}
+
+int	check_goal_meal(t_philo *philo, int i)
+{
+	int	status;
+
+	status = 1;
+	pthread_mutex_lock(&philo->data->lock_eat);
+	if (philo[i].nb_meal == philo->data->iterate)
+		status = 0;
+	pthread_mutex_unlock(&philo->data->lock_eat);
+	return (status);
+}
+
+int	check_time_to_die(t_philo *philo, int i)
+{
+	int	status;
+
+	status = 1;
+	pthread_mutex_lock(&philo->data->lock_eat);
+	if (philo[i].last_meal + philo->data->time_die <= get_time_ms())
+	{
+		philo->data->one_died = 1;
+		status = 0;
+	}
+	pthread_mutex_unlock(&philo->data->lock_eat);
+	return (status);
+}
+
 void	*monitoring(t_philo *philo)
 {
 	int	i;
 
-	while (!philo->data->one_died)
+	while (check_died(philo))
 	{
 		i = 0;
-		pthread_mutex_lock(&philo->data->lock_eat);
-		if (philo->data->finish_eat == philo->data->nb_philo)
-		{
-			pthread_mutex_unlock(&philo->data->lock_eat);
+		if (!check_finish_eat(philo))
 			return (NULL);
-		}
-		pthread_mutex_unlock(&philo->data->lock_eat);
 		while (i < philo->data->nb_philo)
 		{
-			pthread_mutex_lock(&philo->data->lock_eat);
-			if (philo[i].nb_meal == philo->data->iterate)
-			{
-				pthread_mutex_unlock(&philo->data->lock_eat);
+			if (!check_goal_meal(philo, i))
 				break ;
-			}
-			if (philo[i].last_meal + philo->data->time_die <= get_time_ms())
-			{
-				philo->data->one_died = 1;
-				pthread_mutex_unlock(&philo->data->lock_eat);
+			if (!check_time_to_die(philo, i))
 				break ;
-			}
-			pthread_mutex_unlock(&philo->data->lock_eat);
 			i++;
 		}
 		usleep(500);
